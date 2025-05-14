8bcd3ccefb461c5d7274ffb70aa161593fdadbb5
solen80a <sofialennbom@gmail.com>
2025-05-14 13:12:06 +0200

    Moved fetch to app.jsx.
    Co-authored-by: Simon Frisk <simfrisk@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,48 @@ import { MovieList } from "./pages/MovieList";
 import { ShowMovie } from "./pages/ShowMovie";
 import { About } from "./pages/About";
 import { Nav } from "./pages/Nav";
-import { FetchMovieDetails } from "./components/FetchMovieDetails";
+//import { FetchMovieDetails } from "./components/FetchMovieDetails";
 import { Movie } from "./components/Movie";
-
+import { useEffect, useState } from "react";
 
 export const App = () => {
+
+const [movie, setMovie] = useState([]);
+
+  //Something wrong with the .env file?????
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY  
+  //console.log("API Key:", apiKey);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+        if (!response.ok) {
+          throw new Error(`Status ${response.status}`)
+        }
+
+        const data = await response.json()
+          
+        console.log("One movie sample:", data.results[0]);
+        setMovie(data.results);
+
+      } catch (error) {
+        //alert("Oh no, something went wrong!\nThere was an error, please try again later \n" + error)    
+        console.error("Error fetching data:", error)
+      }
+    }
+
+    fetchMovies();
+  }, [apiKey])
+
   return (
     <>
       <Nav />
-      <FetchMovieDetails />
-
+      
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<MovieList />} /> */}
-          <Route path="/movie" element={<ShowMovie />} />
+          <Route path="/" element={<MovieList movie={movie} />} />
+          <Route path="/movie" element={<ShowMovie movie={movie} />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter >
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,7 @@
 //import { Link } from "react-router-dom"
 import { Movie } from "../components/Movie"
 //import ships from "../star_wars_ships.json"
-import { FetchMovieDetails } from "../components/FetchMovieDetails"
+//import { FetchMovieDetails } from "../components/FetchMovieDetails"
 import styled from "styled-components"
 
 const MovieListWrapper = styled.div`
@@ -10,16 +10,12 @@ const MovieListWrapper = styled.div`
   justify-content: center;
   `
 
-export const MovieList = ({movie}) => {
+export const MovieList = ( { movie }) => {
 
 
-  
-
-return (
+  return (
     <>
-    <FetchMovieDetails />
-      {/* <h1>The Popular Movies</h1>  */}
-      <MovieListWrapper>
+    <MovieListWrapper>
         {movie.map((movie) => (
           <Movie
             key={movie.id}
@@ -29,18 +25,6 @@ return (
           />
         ))}
       </MovieListWrapper>
-
     </>
   )
-
-
-//   return (
-//     <>
-// {ships.map((ship) => (
-// <a href="">
-// <Movie name={ship.name} image={ship.image_url} /> 
-// </a>
-// ) )}
-//     </>
-//   )
 }
