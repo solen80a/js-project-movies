e6b5c06addc87e4f2fe446e4c366a35ed777c9d0
solen80a <sofialennbom@gmail.com>
2025-05-15 12:57:10 +0200

    Added a loader to main API fetch.

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,20 @@ import { MovieList } from "./pages/MovieList";
 import { ShowMovie } from "./pages/ShowMovie";
 import { About } from "./pages/About";
 import { Nav } from "./pages/Nav";
-//import { FetchMovieDetails } from "./components/FetchMovieDetails";
 import { Movie } from "./components/Movie";
 import { useEffect, useState } from "react";
+import { Loader } from "./components/Loader";
 
 export const App = () => {
 
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  //Something wrong with the .env file?????
   const apiKey = import.meta.env.VITE_TMDB_API_KEY
-  //console.log("API Key:", apiKey);
-  // {
-  //   movies.map((movie) => (
-  //     console.log(movie.title)
-  //   ))
-  // }
+ 
 
   useEffect(() => {
+    setLoading(true);
     const fetchMovies = async () => {
       try {
         const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
@@ -36,17 +32,24 @@ export const App = () => {
       } catch (error) {
         //alert("Oh no, something went wrong!\nThere was an error, please try again later \n" + error)    
         console.error("Error fetching data:", error)
-      }
+      } finally {
+        setLoading(false);
+      };
     }
 
     fetchMovies();
 
   }, [apiKey])
-  //console.log(title[0])
+ 
+  if (loading) {
+    return (           
+      <Loader /> 
+    )
+  }
   
   return (
     <>
-      <Nav />
+      <Nav />   
 
       <BrowserRouter>
         <Routes>
