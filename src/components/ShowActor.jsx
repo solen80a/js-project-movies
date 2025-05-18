13f49a22a55f9de027675dfba4a835f4bb55bbe6
simfrisk <simoncarlfrisk@gmail.com>
2025-05-18 19:31:48 +0200

    bug fix

--- src/components/ShowActor.jsx
+++ src/components/ShowActor.jsx
@@ -4,15 +4,22 @@ import { getMovieUrls } from "./getMovieUrls";
 import styled from "styled-components";
 import { Link } from "react-router";
 
-const InWhatMovies = styled.div`
+const ActorDetails = styled.div`
+display: flex;
 text-align: center;
 margin: 50px;
+column-gap: 30px;
+align-items: flex-end;
+
+div {
+  text-align: left;
+  width: 50%;
+}
 
 img {
       height: 400px;
 }
 `
-
 
 const Actor = styled.article`
   display: flex;
@@ -43,7 +50,6 @@ const BackToMoviesWrapper = styled(Link)`
   text-align: left;  
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   padding: 1rem ;
-
 `
 
 export const ShowActor = () => {
@@ -52,19 +58,25 @@ export const ShowActor = () => {
   const { movies: moviesIn } = useFetchMovies(actorDetails);
   const { movies: actor } = useFetchMovies(actorPersobalDetailUrl);
 
-
-
   return (
     <>
       <BackToMoviesWrapper to={language === "en-EN" ? "/" : `/${language}`}> ⬅ Back to Movies</BackToMoviesWrapper>
-      <InWhatMovies>
-        <h2>Movies Credits</h2>
+
+
+      <ActorDetails>
         <img src={`https://image.tmdb.org/t/p/original${actor.profile_path}`} alt={actor.name} />
-        <p>{actor.name}</p>
-        <p>{actor.birthday}</p>
-        <p>{actor.place_of_birth}</p>
-        <p>{actor.biography}</p>
-      </InWhatMovies>
+        <div>
+          <p>{actor.name}</p>
+          <p>{actor.birthday}</p>
+          <p>{actor.place_of_birth}</p>
+          <p>
+            {actor.biography?.length > 400
+              ? `${actor.biography.slice(0, 400)}…`
+              : actor.biography}
+          </p>
+        </div>
+      </ActorDetails>
+      <h2>Movies Credits</h2>
       <Actor>
         {moviesIn.slice(0, 10).map(movieIn => (
           <InMoviesCardLink key={movieIn.Id} to={`/movie/${movieIn.id}/${language}`}>
